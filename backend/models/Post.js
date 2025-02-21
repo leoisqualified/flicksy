15b776c4db72d09a2d4bd3a868a77810a4e89a69
leoisqualified <eaadenutsi@st.ug.edu.gh>
2025-02-21 22:50:15 +0000

    backend directory added

---
+++ backend/models/Post.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    mediaUrl: { type: String, required: true },
+    caption: { type: String, default: "" },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", postSchema);
---
+++ backend/models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePicture: { type: String, default: "" },
+    bio: { type: String, default: "" },
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
